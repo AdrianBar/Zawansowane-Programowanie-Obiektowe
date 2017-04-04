a1e093c107991692c8148a00557bd4de100b2e99
AdrianBar <Barszczulimited@gmail.com>
2017-04-04 19:37:47 +0200

    Ćwiczenie 3.2

---
+++ Ćwiczenie 3 - Programowanie Obiektowe/Ćwiczenie 3.2/Ćwiczenie 3.2/KontoBankowe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ćwiczenie_3._2
+{
+    class KontoBankowe
+    {
+        private double _stanKonta;
+        public KontoBankowe(double stankonta)
+        {
+            this._stanKonta = stankonta;
+        }
+
+        public double Wypłata(double ile)
+        {
+            if (_stanKonta > ile)
+            {
+                return _stanKonta = _stanKonta - ile;
+            }
+            else
+               throw new Exception("Nie masz wystarczających środków na koncie");
+        }
+
+        public double Wpłata(double ile)
+        {
+            return _stanKonta = _stanKonta + ile;
+        }
+        public double StanKonta()
+        {
+            return _stanKonta;
+        }
+
+        public void Przelew(KontoBankowe drugieKonto, double ile)
+        {
+            if(_stanKonta > ile)
+            {
+                Wypłata(ile);
+                drugieKonto.Wpłata(ile);
+            }
+        }
+    }
+}
---
+++ Ćwiczenie 3 - Programowanie Obiektowe/Ćwiczenie 3.2/Ćwiczenie 3.2/KontoBankoweTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ćwiczenie_3._2
+{
+    class KontoBankoweTest
+    {
+    }
+}
